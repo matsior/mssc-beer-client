f2e76d00ec4e2de7962d87da236637f7dc5b48d0
Mateusz Krajewski <maatsior@gmail.com>
2023-03-16 21:48:45 +0100

    Initial commit

---
+++ src/test/java/com/matsior/msscbeerclient/MsscBeerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matsior.msscbeerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
